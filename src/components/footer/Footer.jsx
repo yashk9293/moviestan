779ee86e8ff23c82a3dd53aa546b3b1d998f2c3f
yashk9293 <yashk9293@gmail.com>
2024-04-27 10:28:41 +0530

    Moviestan App

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,46 +1,58 @@
 import React from "react";
 import {
-    FaFacebookF,
-    FaInstagram,
-    FaTwitter,
-    FaLinkedin,
+  FaGithub,
+  FaInstagram,
+  FaTwitter,
+  FaLinkedin,
 } from "react-icons/fa";
 
 import ContentWrapper from "../contentWrapper/ContentWrapper";
 
 import "./style.scss";
+import { Link } from "react-router-dom";
 
 const Footer = () => {
-    return (
-        <footer className="footer">
-            <ContentWrapper>
-                <ul className="menuItems">
-                    <li className="menuItem">Terms Of Use</li>
-                    <li className="menuItem">Privacy-Policy</li>
-                    <li className="menuItem">About</li>
-                    <li className="menuItem">Blog</li>
-                    <li className="menuItem">FAQ</li>
-                </ul>
-                <div className="infoText">
-                Moviestan is a web application built using React and Redux that allows users to search for movies and TV shows, view popular, trending, and upcoming releases on a daily and weekly basis, and explore detailed information about each title, including trailers and related videos.
-                </div>
-                <div className="socialIcons">
-                    <span className="icon">
-                        <FaFacebookF />
-                    </span>
-                    <span className="icon">
-                        <FaInstagram />
-                    </span>
-                    <span className="icon">
-                        <FaTwitter />
-                    </span>
-                    <span className="icon">
-                        <FaLinkedin />
-                    </span>
-                </div>
-            </ContentWrapper>
-        </footer>
-    );
+  return (
+    <footer className="footer">
+      <ContentWrapper>
+        <ul className="menuItems">
+          <li className="menuItem">Terms Of Use</li>
+          <li className="menuItem">Privacy-Policy</li>
+          <li className="menuItem">About</li>
+          <li className="menuItem">Blog</li>
+          <li className="menuItem">FAQ</li>
+        </ul>
+        <div className="infoText">
+          Moviestan is a web application built using React and Redux that allows
+          users to search for movies and TV shows, view popular, trending, and
+          upcoming releases on a daily and weekly basis, and explore detailed
+          information about each title, including trailers and related videos.
+        </div>
+        <div className="socialIcons">
+        <Link to={`https://github.com/yashk9293/`} target="_blank">
+          <span className="icon">
+            <FaGithub />
+          </span>
+        </Link>
+        <Link to={`https://www.instagram.com/yashk9293/`} target="_blank">
+          <span className="icon">
+            <FaInstagram />
+          </span>
+        </Link>
+        <Link to={`https://twitter.com/Yashk_9293`} target="_blank">
+          <span className="icon">
+            <FaTwitter />
+          </span>
+        </Link>
+        <Link to={`https://www.linkedin.com/in/yashk9293/`} target="_blank">
+            <span className="icon">
+              <FaLinkedin />
+            </span>
+        </Link>
+        </div>
+      </ContentWrapper>
+    </footer>
+  );
 };
 
 export default Footer;
